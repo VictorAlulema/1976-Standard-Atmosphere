8ccfccd6c450168574332e04f09b26311945062b
Victor Hugo Alulema <victor.h-g@hotmail.es>
2020-04-23 18:50:16 -0500

    Add files via upload

--- example_std_atmosphere.py
+++ example_std_atmosphere.py
@@ -1,7 +1,8 @@
-from standard_atmosphere import *
+import standard_atmosphere as std
+
 
 # Initialize an object
-flight =  std_atmosphere(2850)        # One input: z - altitude
+flight = std.std_atmosphere(2850)     # One input: z - altitude
 
 # Density
 print('Altitude {} [m]'.format(flight.z))
@@ -12,22 +13,23 @@ print(flight.sound_speed())
 print(flight.dynamic_pressure(10))    # One input: air speed
 print(flight.viscosity_dynamic())
 print(flight.viscosity_kinematic())
-print(flight.Reynolds(0.305,10))        # two inputs: reference length and air speed
+print(flight.Reynolds(0.305, 10))     # two inputs: ref. length and air speed
 print(flight.Mach(50))                # One input: air speed
 
 # Perform multipoint analysis
-Z = [0,1500,3000,4500,10000]          # list of flight altitudes
+Z = [0, 1500, 3000, 4500, 10000]      # list of flight altitudes
 
 # Density value for various flight altitudes
-densities =  [std_atmosphere(z).density() for z in Z]
+densities = [std.std_atmosphere(z).density() for z in Z]
 
 # Mach number for various flight speeds at a fixed flight altitude
 # range of flight speeds range from 10 to 50 with steps of 5
-Mach_numbers = [std_atmosphere(1500).Mach(u) for u in range(10,50,5)]
+Mach_numbers = [std.std_atmosphere(1500).Mach(u) for u in range(10, 50, 5)]
 
 # Reynolds number at differente flight speeds and altitudes
 
-Z = [0,1500,3000,4500,10000]   # For each altitude, there is a corresponding flight speed
-U = [30,35,40,45,50]
+Z = [0, 1500, 3000, 4500, 10000]     # For each z, there is a flight speed
+U = [30, 35, 40, 45, 50]
 
-Reynolds_numbers = [std_atmosphere(z).Reynolds(0.2,U[x]) for x,z in enumerate(Z)]
+Reynolds_numbers = [std.std_atmosphere(z).Reynolds(0.2, U[x])
+                    for x, z in enumerate(Z)]
--- standard_atmosphere.py
+++ standard_atmosphere.py
@@ -1,88 +1,98 @@
-from math import exp,sqrt
+from math import exp, sqrt
+
 
 class std_atmosphere:
-    """ 1976 Standard Atmosphere:
-        z: [m] 
-        Reference:
-        http://www.dept.aoe.vt.edu/~mason/Mason_f/stdatm.f
     """
-    def __init__(self,z):
-        self.z   = z                  # Flight altitude
-        self.K   = 34.163195          # Constant
-        self.Tsl = 288.15             # Temp. sea level
-        self.Psl = 101325             # Press. sea level
-        self.Rsl = 1.225              # Density sea level
-        self.C1  = .001               # Factor: m to Km
-        self.Asl = 340.294            # Sound speed sea level
-        self.Msl = 1.7894E-05         # Viscosity sea level
-        self.BT  = 1.458E-06          # Beta constant for viscosity eq. 
-        self.T , self.P = std_atmosphere.std_atmos_model(self)
+    1976 Standard Atmosphere:
+    z: [m]
+    Reference:
+    http://www.dept.aoe.vt.edu/~mason/Mason_f/stdatm.f
+    """
+
+    def __init__(self, z):
+        self.z = z                  # Flight altitude
+        self.T, self.P = std_atmosphere.std_atmos_model(self)
         self.rho = std_atmosphere.density(self)
-        
+
     def std_atmos_model(self):
         """1976 Standard Atmosphere model"""
-        H = self.C1 * self.z / (1 + self.C1 * self.z / 6356.766)
+        K = 34.163195          # Constant
+        C1 = .001               # Factor: m to Km
+        H = C1 * self.z / (1 + C1 * self.z / 6356.766)
         if H < 11:
-            T  = 288.15 - 6.5 * H
-            P  = (288.15 / T) ** ( - self.K / 6.5)
+            T = 288.15 - 6.5 * H
+            P = (288.15 / T) ** (- K / 6.5)
         elif H < 20:
-            T  = 216.65
-            P  = 0.22336 * exp( - self.K * (H - 11) / 216.65)
+            T = 216.65
+            P = 0.22336 * exp(- K * (H - 11) / 216.65)
         elif H < 32:
-            T  = 216.65 + (H - 20)
-            P  = 0.054032 * (216.65 / T) ** self.K
+            T = 216.65 + (H - 20)
+            P = 0.054032 * (216.65 / T) ** K
         elif H < 47:
-            T  = 228.65 + 2.8 * (H - 32)
-            P  = .0085666 * (228.65 / T) ** (self.K / 2.8)
+            T = 228.65 + 2.8 * (H - 32)
+            P = .0085666 * (228.65 / T) ** (K / 2.8)
         elif H < 51:
-            T  = 270.65
-            P  = .0010945 * exp( - self.K * (H - 47) / 270.65)
+            T = 270.65
+            P = .0010945 * exp(- K * (H - 47) / 270.65)
 
         elif H < 71:
-            T  = 270.65 - 2.8 * (H - 51)
-            P  = .00066063 * (270.65 / T) ** ( - self.K / 2.8)
+            T = 270.65 - 2.8 * (H - 51)
+            P = .00066063 * (270.65 / T) ** (- K / 2.8)
         elif H < 84.852:
-            T  = 214.65 - 2 * (H - 71)
-            P  = 3.9046e-5 * (214.65 / T) ** (- self.K / 2)
+            T = 214.65 - 2 * (H - 71)
+            P = 3.9046e-5 * (214.65 / T) ** (- K / 2)
         else:
             error = 'z:{} [m] ouf of limits for S.A.'.format(self.z)
             raise Exception(error)
-        return T , P 
+        return T, P
+
     def temperature(self):
         """Temperature at "z" altitude """
+        Tsl = 288.15                 # Temp. sea level
         T_ratio = self.T / 288.15
-        return self.Tsl * T_ratio
+        return Tsl * T_ratio
+
     def pressure(self):
         """Pressure at "z" altitude """
-        return self.Psl * self.P
+        Psl = 101325                 # Press. sea level
+        return Psl * self.P
+
     def density(self):
         """Density at "z" altitude """
-        R_ratio  = self.P / (self.T / 288.15)
-        return self.Rsl * R_ratio
+        Rsl = 1.225                  # Density sea level
+        R_ratio = self.P / (self.T / 288.15)
+        return Rsl * R_ratio
+
     def sound_speed(self):
         """Sound speed at "z" altitude """
+        Asl = 340.294                # Sound speed sea level
         T_ratio = self.T / 288.15
-        return self.Asl * sqrt(T_ratio)
-    def dynamic_pressure(self,U):
-        """Dynamic pressure at "z" altitude
-           U: air speed [m/s] 
+        return Asl * sqrt(T_ratio)
+
+    def dynamic_pressure(self, U):
         """
-        R = std_atmosphere.density(self)
-        return (R * U **2) / 2
+        Dynamic pressure at "z" altitude
+        U: air speed [m/s]
+        """
+        return (self.rho * U ** 2) / 2
+
     def viscosity_dynamic(self):
         """Dynamic viscosity - Sutherland Equation"""
-        return self.BT * self.T ** 1.5 / (self.T + 110.4)
+        BT = 1.458E-06              # Beta constant for viscosity eq.
+        return BT * self.T ** 1.5 / (self.T + 110.4)
+
     def viscosity_kinematic(self):
         u = std_atmosphere.viscosity_dynamic(self)
         return u / self.rho
-    def Reynolds(self,x,U):
+
+    def Reynolds(self, x, U):
         """Reynolds number
            U: air speed [m/s]
            x: reference length [m] (airfoil/wing chord)
         """
         u = std_atmosphere.viscosity_dynamic(self)
         return (self.rho * U * x) / u
-    def Mach(self,U):
+
+    def Mach(self, U):
         A = std_atmosphere.sound_speed(self)
         return U / A
-
